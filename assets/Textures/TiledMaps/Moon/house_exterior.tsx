e1dba7b5e23bb61e7810a07c520c5e02a854239f
Acquati <leandroacquati@gmail.com>
2022-04-28 19:09:15 -0300

    add buildings exterior

---
+++ assets/Textures/TiledMaps/Moon/house_exterior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="house_exterior" tilewidth="16" tileheight="16" tilecount="350" columns="14">
+ <image source="house_exterior.png" width="224" height="400"/>
+</tileset>
---
+++ assets/Textures/TiledMaps/Moon/police_station_exterior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="police_station_exterior" tilewidth="16" tileheight="16" tilecount="576" columns="24">
+ <image source="police_station_exterior.png" width="384" height="384"/>
+</tileset>
