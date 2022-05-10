d6d480086f6c8076ddc1fd22d632e9243bc8722a
Acquati <leandroacquati@gmail.com>
2022-05-10 20:03:35 -0300

    update bar interior map

--- assets/Textures/TiledMaps/Bar/bar_interior.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="bar_interior" tilewidth="16" tileheight="16" tilecount="858" columns="33">
- <image source="bar_interior.png" width="528" height="416"/>
-</tileset>
