e6cc64544f1760858f47ee87ed13f1c4411131d6
Samir Kazah <samir.kazah@gmail.com>
2022-05-11 10:31:11 +0300

    Updating sprites and tiles

--- assets/Textures/TiledMaps/MoonBase/house_exterior.tsx
+++ assets/Textures/TiledMaps/MoonBase/house_exterior.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="house_exterior" tilewidth="16" tileheight="16" tilecount="350" columns="14">
- <image source="house_exterior.png" width="224" height="400"/>
+<tileset version="1.5" tiledversion="1.7.2" name="house_exterior" tilewidth="16" tileheight="16" tilecount="468" columns="18">
+ <image source="house_exterior.png" width="288" height="416"/>
 </tileset>
