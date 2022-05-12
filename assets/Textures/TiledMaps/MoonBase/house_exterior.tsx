830cbbf82d4e8940d95476073c6b6b856e2f7406
Samir Kazah <samir.kazah@gmail.com>
2022-05-12 12:34:43 +0300

    Updating sprites and tiles, Added placeholder bank tiles

--- assets/Textures/TiledMaps/MoonBase/house_exterior.tsx
+++ assets/Textures/TiledMaps/MoonBase/house_exterior.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="house_exterior" tilewidth="16" tileheight="16" tilecount="468" columns="18">
+<tileset version="1.8" tiledversion="1.8.4" name="house_exterior" tilewidth="16" tileheight="16" tilecount="468" columns="18">
  <image source="house_exterior.png" width="288" height="416"/>
 </tileset>
