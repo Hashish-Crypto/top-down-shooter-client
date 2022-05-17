58a7d5073c49bdc8a9cb58f3d15040b2edee3a25
Samir Kazah <samir.kazah@gmail.com>
2022-05-17 19:19:29 +0300

    Updated Bar and Police Station interior

---
+++ assets/Textures/Items/Projector/projector.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="projector" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="projector_tiles.png" width="128" height="128"/>
+</tileset>
