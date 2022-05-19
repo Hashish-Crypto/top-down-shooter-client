d9141dd1cc26b0fbbb19d7df50d1c235e50c7bb8
Samir Kazah <samir.kazah@gmail.com>
2022-05-20 00:24:47 +0300

    Added dungeon interior tilemap, deleted unused art

--- assets/Textures/TiledMaps/House/lounge_ref.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="lounge_ref" tilewidth="16" tileheight="16" tilecount="1900" columns="38">
- <image source="lounge_ref.png" width="608" height="800"/>
-</tileset>
