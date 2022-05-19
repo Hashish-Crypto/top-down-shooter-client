f92a8aaecd5668866bd70e1321f5ef2a637c1385
Samir Kazah <samir.kazah@gmail.com>
2022-05-19 16:01:29 +0300

    Updating Lounge interior

---
+++ assets/Textures/TiledMaps/House/lounge_interior_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="lounge_interior_tiles" tilewidth="16" tileheight="16" tilecount="676" columns="20">
+ <image source="lounge_interior_tiles.png" width="320" height="320"/>
+</tileset>
