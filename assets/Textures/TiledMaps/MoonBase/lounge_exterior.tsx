c12b5e0cb2f7a84c930e9b1b957b85bbf79f57a5
Acquati <leandroacquati@gmail.com>
2022-05-30 15:40:59 -0300

    fix moon base tiledmap

--- assets/Textures/TiledMaps/MoonBase/lounge_exterior.tsx
+++ assets/Textures/TiledMaps/MoonBase/lounge_exterior.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="house_exterior" tilewidth="16" tileheight="16" tilecount="468" columns="18">
- <image source="house_exterior.png" width="288" height="416"/>
+<tileset version="1.8" tiledversion="1.8.4" name="lounge_exterior" tilewidth="16" tileheight="16" tilecount="468" columns="18">
+ <image source="lounge_exterior.png" width="288" height="416"/>
 </tileset>
