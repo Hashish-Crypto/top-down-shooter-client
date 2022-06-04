beaea27388249d38df15761f6e3819992faf7f1d
Samir Kazah <samir.kazah@gmail.com>
2022-06-04 18:54:19 +0300

    Updated bank interior tiles

---
+++ assets/Textures/TiledMaps/Bank/bank_interior_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="bank_interior_tiles" tilewidth="16" tileheight="16" tilecount="324" columns="18">
+ <image source="bank_interior_tiles.png" width="288" height="288"/>
+</tileset>
