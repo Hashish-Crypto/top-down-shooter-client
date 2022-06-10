ad6dc6a403be3bdf6ddac9537c43ea6e520f5a87
Samir Kazah <samir.kazah@gmail.com>
2022-06-10 12:02:06 +0300

    Added shop exterior to moon base

--- assets/Textures/TiledMaps/MoonBase/shop_exterior.tsx
+++ assets/Textures/TiledMaps/MoonBase/shop_exterior.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="shop exterior" tilewidth="16" tileheight="16" tilecount="1728" columns="24">
- <image source="shop_exterior.png" width="384" height="288"/>
+<tileset version="1.8" tiledversion="1.8.4" name="shop exterior" tilewidth="16" tileheight="16" tilecount="180" columns="10">
+ <image source="shop_exterior.png" width="160" height="288"/>
 </tileset>
